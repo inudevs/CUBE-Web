dcc832b1c4cdc87d0849af9d0cf94bb2538a905b
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-04-20 22:58:24 +0900

    feat: profile

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -15,6 +15,14 @@ export default {
         email: 'jyeo@civar.dev'
       }
     }
+  },
+  methods: {
+    changePassword () {
+      return 1
+    },
+    logout () {
+      this.$router.push({ name: 'Intro' })
+    }
   }
 }
 </script>
@@ -24,21 +32,21 @@ export default {
     <app-title />
     <div class="profile">
       <img src="https://avatars0.githubusercontent.com/u/32605822?s=460">
-      <div class="about">
-        <div class="about__menu">
-          <div class="field">이름</div>
-          <div class="value">{{ user.name }}</div>
-        </div>
-        <div class="about__menu">
-          <div class="field">이메일</div>
-          <div class="value">{{ user.email }}</div>
-        </div>
-        <div class="about__menu">
-          <div class="setting">비밀번호 변경</div>
-        </div>
-        <div class="about__menu">
-          <div class="setting">로그아웃</div>
-        </div>
+    </div>
+    <div class="about">
+      <div class="about__menu">
+        <div class="field">이름</div>
+        <div class="value">{{ user.name }}</div>
+      </div>
+      <div class="about__menu">
+        <div class="field">이메일</div>
+        <div class="value">{{ user.email }}</div>
+      </div>
+      <div class="about__menu" @click="changePassword">
+        <div class="setting">비밀번호 변경</div>
+      </div>
+      <div class="about__menu" @click="logout">
+        <div class="setting">로그아웃</div>
       </div>
     </div>
     <app-menu current="gear" />
@@ -58,13 +66,60 @@ export default {
 }
 
 .profile {
+  width: 100%;
+  text-align: center;
+  position: relative;
+
   img {
+    margin: auto;
+    margin-top: 1em;
     width: 12%;
     border-radius: 50%;
 
     @media (max-width: 520px) {
       width: 30%;
     }
+  }
+}
+
+.about {
+  width: 60%;
+  margin: auto;
+  text-align: left;
+  padding: 0.5em;
+  font-size: 1.2em;
+
+  @media (max-width: 520px) {
+    width: 90%;
+  }
+
+  &__menu {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  &__menu:not(:last-child) {
+    border: none;
+    border-bottom: 1px solid #d3d3d3;
+  }
+
+  .field, .setting {
+    color: #707070;
+    padding: 1em;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .setting {
+    width: 100%;
+  }
+
+  .value {
+    color: #707070;
+    padding: 1em;
+    text-align: right;
+    justify-content: flex-start;
   }
 }
 </style>
