1b3c1b5b38a915161e88e853831de7bac8874115
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-02 22:46:00 +0900

    chore: update changed path in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/CUBE-App/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/CUBE-Web/' : '/'
 }
